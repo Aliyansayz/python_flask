d2ba93c285c7ec34a0d96bdc3904feeb4180b1ff
Aliyan Anwar <56589240+Aliyansayz@users.noreply.github.com>
2023-05-03 15:02:08 +0500

    Create cheatsheet_sql .sql

---
+++ cheatsheet_sql .sql
@@ -0,0 +1,79 @@
+Here is an SQL cheat sheet for an intermediate Python Flask backend developer:
+
+### CREATE TABLE
+
+```SQL
+CREATE TABLE table_name (
+    column1 datatype,
+    column2 datatype,
+    column3 datatype
+);
+```
+
+### INSERT INTO
+
+```SQL
+INSERT INTO table_name (column1, column2, column3) VALUES (value1, value2, value3);
+```
+
+### SELECT
+
+```SQL
+SELECT column1, column2, column3 FROM table_name;
+```
+
+### WHERE
+
+```SQL
+SELECT column1, column2, column3 FROM table_name WHERE condition;
+```
+
+### UPDATE
+
+```SQL
+UPDATE table_name SET column1 = new_value WHERE condition;
+```
+
+### DELETE
+
+```SQL
+DELETE FROM table_name WHERE condition;
+```
+
+### ORDER BY
+
+```SQL
+SELECT column1, column2, column3 FROM table_name ORDER BY column1 ASC/DESC;
+```
+
+### GROUP BY
+
+```SQL
+SELECT column1, AVG(column2), COUNT(*) FROM table_name GROUP BY column1;
+```
+
+### JOIN
+
+```SQL
+SELECT column1, column2, column3 FROM table1 JOIN table2 ON table1.column1 = table2.column1;
+```
+
+### LIMIT
+
+```SQL
+SELECT column1, column2, column3 FROM table_name LIMIT 10;
+```
+
+### ALTER TABLE
+
+```SQL
+ALTER TABLE table_name ADD COLUMN column_name datatype;
+```
+
+### DROP TABLE
+
+```SQL
+DROP TABLE table_name;
+```
+
+These are some of the most commonly used SQL statements that you may need to use in your Python Flask backend development work. Remember to always use parameterized queries to prevent SQL injection attacks, and to test your queries thoroughly before deploying them to production.
